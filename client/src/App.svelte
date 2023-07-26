7ed31b9dcb8b7cb1b6f44583b82c17275c3dd6ca
WariHue <warihue@gmail.com>
2023-07-26 18:35:28 +0900

    ⭐거지같다⭐

---
+++ client/src/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import Card from "./lib/Card.svelte";
+  let myTypeOfCard = "Spade";
+  let myNumberOfCard = 1
+  const click = (type:string): void => {
+    myTypeOfCard= type
+    myNumberOfCard++
+  }
+</script>
+
+<main>
+  <hi class="j">{myTypeOfCard}!</hi>
+  <button on:click={() => {click("Heart")}}>a</button>
+  <Card type={myTypeOfCard} num={myNumberOfCard}/>
+</main>
+
+<style>
+  .j {
+    color: blue;
+    font-size: 10rem;
+  }
+</style>
---
+++ client/src/lib/Card.svelte
@@ -0,0 +1,6 @@
+<script>
+    export let type, num
+</script>
+
+<img src="/{type}-{num}.png" alt="1" width="300px"/>
+<h1>{type}</h1>
